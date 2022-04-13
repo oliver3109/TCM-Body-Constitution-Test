b8f0444f59bc66df5e52b18991d056aed4f3c3ee
Oliver <810982048@qq.com>
2022-04-13 19:34:33 +0800

    chore: 删除多余包

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 import { defineNuxtConfig } from "nuxt3";
-import Components from "unplugin-vue-components/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
