0952c9ef84a42dad0b872cfb4046af3f493cc949
Oliver <810982048@qq.com>
2022-04-14 18:02:33 +0800

    fix: vant按需引入

--- plugins/vant.ts
+++ plugins/vant.ts
@@ -1,6 +1,9 @@
-import vant from "vant";
+import { Tab, Tabs } from "vant";
+
+// 目前在 nuxt 中无法按需引入样式，因此采用手动引入的方式
 import "vant/lib/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(vant);
+  nuxtApp.vueApp.use(Tab);
+  nuxtApp.vueApp.use(Tabs);
 });
